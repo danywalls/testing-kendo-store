fb1d4868e79a3bee2a41b37cd6412e22d34cd5d1
Dany Paredes <danywalls4@gmail.com>
2025-04-12 23:50:04 +0200

    test: 🧪 clean up formatting in app component tests for consistency

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
-
 import {AppComponent} from "./app.component";
-import { ProductsService} from "./services/products.service";
+import {ProductsService} from "./services/products.service";
 import {of} from "rxjs";
 import {MOCK_PRODUCTS} from "./tests/mock";
 import {render, screen} from "@testing-library/angular";
@@ -12,12 +11,13 @@ export class MockProductService {
 
 describe('app component', () => {
 
-  it('should render the product', async ()  => {
+  it('should render the product', async () => {
 
     await render(AppComponent, {
-      providers: [{ provide: ProductsService, useClass: MockProductService }],
+      providers: [{provide: ProductsService, useClass: MockProductService}],
     })
 
-    const productTitle  = screen.getByText(MOCK_PRODUCTS[0].title);
+    const productTitle = screen.getByText(MOCK_PRODUCTS[0].title);
     expect(productTitle).toBeDefined()
-})});
+  })
+});
