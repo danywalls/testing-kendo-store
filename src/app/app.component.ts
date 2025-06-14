ac03643e33ede0250d0ff0c38cfb2adf64da0f61
Dany <danywalls4@gmail.com>
2025-06-14 23:58:21 +0200

    chore: 🔧 fix formatting in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import {ProductsService} from "./services/products.service";
 import {AsyncPipe, CurrencyPipe} from "@angular/common";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, AsyncPipe, CurrencyPipe],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, AsyncPipe, CurrencyPipe],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   public title = 'testing-in-angular';
